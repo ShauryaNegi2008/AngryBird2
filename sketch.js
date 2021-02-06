d923bc8ff970ccd0dceede038ae166028b7fbb0a
ShauryaNegi2008 <NegiShaurya208@gmail.com>
2021-02-06 19:33:40 +0530

    Debugging

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
+var box1, pig1, pig3;
+var backgroundImg, platform;
 var bird, slingshot;
-var bg="sprites/bg.png"
-var score=0;
+var bg = "sprites/bg.png"
+var score = 0;
 var gameState = "onSling";
 
 function preload() {
@@ -16,44 +16,44 @@ function preload() {
 
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
+function setup() {
+    var canvas = createCanvas(1200, 400);
     engine = Engine.create();
     world = engine.world;
 
 
-    ground = new Ground(600,height,1200,20);
+    ground = new Ground(600, height, 1200, 20);
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
+    box1 = new Box(700, 320, 70, 70);
+    box2 = new Box(920, 320, 70, 70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    log1 = new Log(810, 260, 300, PI / 2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
+    box3 = new Box(700, 240, 70, 70);
+    box4 = new Box(920, 240, 70, 70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log3 = new Log(810, 180, 300, PI / 2);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    box5 = new Box(810, 160, 70, 70);
+    log4 = new Log(760, 120, 150, PI / 7);
+    log5 = new Log(870, 120, 150, -PI / 7);
 
-    bird = new Bird(200,50);
+    bird = new Bird(200, 50);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
-    
+    slingshot = new SlingShot(bird.body, { x: 200, y: 50 });
+
 }
 
-function draw(){
-    if(backgroundImg)
-    background(backgroundImg);
+function draw() {
+    if (backgroundImg)
+        background(backgroundImg);
     noStroke();
     textSize(35);
     fill("white");
-    text("score "+score,width-300,50);
+    text("score " + score, width - 300, 50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -76,38 +76,40 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();
 }
 
-function mouseDragged(){
-    if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    }
+function mouseDragged() {
+    // if (gameState!=="launched"){
+    Matter.Body.setPosition(bird.body, { x: mouseX, y: mouseY });
+    // }
 }
 
 
-function mouseReleased(){
+function mouseReleased() {
     slingshot.fly();
     gameState = "launched";
 }
 
-function keyPressed(){
-    if(keyCode === 32){
-       // slingshot.attach(bird.body);
+function keyPressed() {
+    if (keyCode === 32) {
+        bird.trajectory = []
+        Matter.Body.setPosition(bird.body, { x: 200, y: 50 })
+        slingshot.attach(bird.body);
     }
 }
-async function getTime(){
-    var response=await fetch("https://worldtimeapi.org/api/timezone/Asia/kolkata")
+async function getTime() {
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/kolkata")
     console.log(response)
-    var responsejson=await response.json();
+    var responsejson = await response.json();
     console.log(responsejson)
-var datetime=responsejson.datetime
-var hour = datetime.slice(11,13)
-console.log(hour)
-if(hour>=06 && hour<=19){
-    bg="sprites/bg.png"
-}else{
-    bg="sprites/bg2.jpg"
-}
-backgroundImg=loadImage(bg)
+    var datetime = responsejson.datetime
+    var hour = datetime.slice(11, 13)
+    console.log(hour)
+    if (hour > 06 && hour < 19) {
+        bg = "sprites/bg.png"
+    } else {
+        bg = "sprites/bg2.jpg"
+    }
+    backgroundImg = loadImage(bg)
 }
